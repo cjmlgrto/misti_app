e609cd51a57bb37dfc28a56f01735f5832b660b7
Carlos Melegrito <cj@mlgrto.com>
2023-08-23 23:11:22 +1000

    Add Permission Handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 import 'package:app_template/controller.dart';
 
 void main() {
-  runApp(const MainApp());
+  if (Platform.isAndroid) {
+    WidgetsFlutterBinding.ensureInitialized();
+    [
+      Permission.location,
+      Permission.storage,
+      Permission.bluetooth,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan
+    ].request().then((status) {
+      runApp(const MainApp());
+    });
+  } else {
+    runApp(const MainApp());
+  }
 }
 
 class MainApp extends StatelessWidget {
