a0d2f666c7fd921105bf1b15af98034b9136b528
Carlos Melegrito <cj@mlgrto.com>
2023-08-23 23:06:21 +1000

    Set up basic companion app

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
-import 'package:app_template/screens/status.dart';
+import 'package:app_template/view.dart';
+import 'package:app_template/model.dart';
 
 class BluetoothController extends StatelessWidget {
   const BluetoothController({super.key});
@@ -34,14 +37,13 @@ class DeviceScanner extends StatelessWidget {
     FlutterBlue.instance.startScan(timeout: const Duration(seconds: 4));
     FlutterBlue.instance.scanResults.listen((results) {
       for (ScanResult r in results) {
-        print(r.device.name);
-        if (r.device.name == "Arduino") {
+        if (r.device.name == Device.name) {
           FlutterBlue.instance.stopScan();
           r.device.connect();
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => const StatusScreen(text: "Connected")));
+                  builder: (context) => DeviceScreen(device: r.device)));
         }
       }
     });
@@ -49,5 +51,82 @@ class DeviceScanner extends StatelessWidget {
     FlutterBlue.instance.stopScan();
 
     return const StatusScreen(text: "Connecting...");
+  }
+}
+
+// Device Screen
+// ------------------------------------------------------------
+class DeviceScreen extends StatefulWidget {
+  final BluetoothDevice device;
+
+  const DeviceScreen({Key? key, required this.device}) : super(key: key);
+
+  @override
+  State<DeviceScreen> createState() => _DeviceScreenState();
+}
+
+class _DeviceScreenState extends State<DeviceScreen> {
+  late Timer _everySecond;
+
+  int exampleCharacteristic = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _everySecond = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() {
+        refresh();
+      });
+    });
+  }
+
+  Future refresh() async {
+    widget.device.discoverServices().then((services) => {
+          services.forEach((service) async {
+            if (service.uuid.toString() == "".toLowerCase()) {
+              for (BluetoothCharacteristic c in service.characteristics) {
+                List<int> value = await c.read();
+
+                // exampleCharacteristic
+                if (c.uuid.toString() ==
+                    Device.exampleCharacteristic.toLowerCase()) {
+                  setState(() {
+                    exampleCharacteristic = value[0];
+                  });
+                }
+              }
+            }
+          })
+        });
+
+    await Future.delayed(const Duration(seconds: 1));
+  }
+
+  void reset() async {
+    List<BluetoothService> services = await widget.device.discoverServices();
+
+    services.forEach((service) async {
+      var characteristics = service.characteristics;
+      for (BluetoothCharacteristic c in characteristics) {
+        // Reset Characteristic
+        if (c.uuid.toString() == Device.resetCharacteristic.toLowerCase()) {
+          c.write([0]);
+        }
+      }
+    });
+
+    setState(() {
+      exampleCharacteristic = 0;
+    });
+  }
+
+  void exit() {
+    widget.device.disconnect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StatusScreen(text: "exampleCharacteristic: $exampleCharacteristic");
   }
 }
---
+++ lib/model.dart
@@ -0,0 +1,7 @@
+class Device {
+  static const String name = "Aeolus_alpha_1";
+  static const String exampleCharacteristic =
+      "20B10023-E8F2-537E-4F6C-D104768A1214";
+  static const String resetCharacteristic =
+      "20B10028-E8F2-537E-4F6C-D104768A1214";
+}
