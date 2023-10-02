81305a423e621b02a2f7f8181678c86335496079
Carlos Melegrito <cj@mlgrto.com>
2023-10-02 17:43:44 +1100

    Set up device name

--- lib/model.dart
+++ lib/model.dart
@@ -1,5 +1,5 @@
 class Device {
-  static const String name = "Ocumate_POC_1";
+  static const String name = "Misti_Mask";
   static const String exampleCharacteristic =
       "20B10023-E8F2-537E-4F6C-D104768A1214";
   static const String resetCharacteristic =
