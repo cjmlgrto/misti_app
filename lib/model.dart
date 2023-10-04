b7877709d87c800c016967f617d89716c01b8b15
Carlos Melegrito <cj@mlgrto.com>
2023-10-04 14:46:25 +1100

    Add firmware IDs

--- lib/model.dart
+++ lib/model.dart
@@ -1,9 +1,21 @@
 class Device {
+  // The local device name
   static const String name = "My_Misti_01";
-  static const String exampleCharacteristic =
-      "20B10023-E8F2-537E-4F6C-D104768A1214";
-  static const String resetCharacteristic =
-      "20B10028-E8F2-537E-4F6C-D104768A1214";
+
+  // myMistiService
+  static const String service = "20B10020-E8F2-537E-4F6C-D104768A1214";
+
+  // The number of drops recorded by the device
+  static const String dosageCharacteristic = "MST-C01-A";
+
+  // The battery level of the device from 0-100
+  static const String batteryLevelCharacteristic = "MST-C02-A";
+
+  // The theoretical number of drops left in the bottle
+  static const String dispenseCharacteristic = "MST-C03-A";
+
+  // Reset function
+  static const String resetCharacteristic = "MST-C04-A";
 }
 
 enum DeviceState { connected, connecting, disconnected, off }
