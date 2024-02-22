fd2f13db7021e7a584dceb93cc3dd3514d48afa8
Carlos Melegrito <cj@mlgrto.com>
2024-02-23 09:44:48 +1100

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:misti/screens/demo.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'package:misti/controller.dart';
